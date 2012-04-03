e143f7b89cd127bb7ae0835c7485b4383693c343
harshit patel <hp12180@yahoo.com>
2012-04-03 23:25:14 +0530

    3

--- js/catlist.js
+++ js/catlist.js
@@ -6,7 +6,7 @@ var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScro
 var employees;
 
 $(window).load(function() {
-	setTimeout(getEmployeeList, 100);
+	setTimeout(getcatList, 100);
 });
 
 $(document).ajaxError(function(event, request, settings) {
@@ -14,14 +14,14 @@ $(document).ajaxError(function(event, request, settings) {
 	alert("Error accessing the server");
 });
 
-function getEmployeeList() {
+function getcatList() {
 	$('#busy').show();
 	$.getJSON(serviceURL + 'getsubcats.php', function(data) {
 		$('#busy').hide();
-		$('#employeeList li').remove();
+		$('#catList li').remove();
 		employees = data.items;
 		$.each(employees, function(index, employee) {
-			$('#employeeList').append('<li><a href="subcategories.html?id=' + employee.categories_id + '">' +
+			$('#catList').append('<li><a href="subcategories.html?id=' + employee.categories_id + '">' +
 					'<p class="line1">' + employee.categories_name + '</p>');
 		});
 		setTimeout(function(){
