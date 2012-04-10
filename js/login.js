74d12d34e855a5a164131721155324f0d5c360ce
harshit patel <hp12180@yahoo.com>
2012-04-11 00:18:10 +0530

    1

--- js/login.js
+++ js/login.js
@@ -1,26 +1,29 @@
-var loginURL = localStorage['serviceURL'];
+var id = 'hp12180@yahoo.com';
+var pwd = 'getright1';
+var serviceURL = localStorage['serviceURL'];
 
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
-var logindet;
-var id = 'hp12180@yahoo.com';
-var pwd = 'getright1';
+var userdetails;
+
 $(window).load(function() {
-	setTimeout(getLogin, 100);
+	setTimeout(getcatList, 100);
 });
 
 $(document).ajaxError(function(event, request, settings) {
 	$('#busy').append('Error accessing the server');
 });
 
-function getLogin() {
+function getcatList() {
 	$('#busy').show();
-	$.getJSON(loginURL + 'login.php?id=' + id + '&pwd=' + pwd, function(data) {
+	$.getJSON(serviceURL + 'login.php?id='+id+'&pwd='+pwd, function(data) {
 		$('#busy').hide();
 		$('#userDet li').remove();
-		logindet = data.items;
-		$('#userDet').append('<li>' +
-					'<p class="line1">' + logindet + '</p>');
+		userdetails = data.items;
+		$.each(userdetails, function(index, userdetail) {
+			$('#userDet').append('<li>' + userdetail.login_status +
+					'<p class="line1">' + userdetail.login_message + '</p>');
+		});
 		setTimeout(function(){
 			scroll.refresh();
 		});
