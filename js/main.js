6b9dca3d1773f46064b709cfab59c65496533323
Harshit Patel <hp12180@yahoo.com>
2012-04-14 15:40:50 +0530

    w

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScro
 $(window).load(function() {
 	setTimeout(getmainbanners, 100);
 });
-			
-			
+		
 function getmainbanners() {
 	$('#busy').show();
 	$('#busy').hide();
-		setTimeout(function(){
-			scroll.refresh();
-		});
+	$('#mainDet li').remove();
+	$('#mainDet').append('<div class="tile"><a href="index.html">Top Ten Toys</a></div><div class="tile"><a href="index.html">Top Ten Categories</a></div><div class="tile"><a href="index.html">Shop by Brand</a></div><div class="tile"><a href="index.html">Shop by Category</a></div><div class="tile"><a href="index.html">Shop by Age</a></div><div class="tile"><a href="index.html">Shop by Price</a></div>	<div class="tile"><a href="index.html">Contact Us</a></div><div class="tile"><a href="index.html">My Account</a></div><div class="tile"><a href="index.html">Shopping Cart</a></div>');	
+	setTimeout(function(){
+		scroll.refresh();
+	});
 }
