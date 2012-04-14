3e4fff2b97ee09f8c2fe832d4e85e29c0e19ff79
Harshit Patel <hp12180@yahoo.com>
2012-04-14 14:23:44 +0530

    23

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
+$(window).load(function() {
+	setTimeout(getmainbanners, 100);
+});
+			
+			
+function getmainbanners() {
+	$('#busy').show();
+	$('#busy').hide();
+		setTimeout(function(){
 			scroll.refresh();
-			
-			
+		});
+}
