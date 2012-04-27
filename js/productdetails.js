2de1a553af24f874e9cc132b8275454c06008de3
Harshit Patel <hp12180@yahoo.com>
2012-04-27 17:56:26 +0530

    2

--- js/productdetails.js
+++ js/productdetails.js
@@ -28,6 +28,19 @@ function getprodDetails() {
 			{
 			picons = picons + '<img src="img/icons/icon_bargain.gif">';
 			}
+			if (pdetail.products_quantity < 1)
+			{
+			picons = picons + '<img src="img/icons/icon_stop.gif">';
+			}
+			var t = pdetail.products_date_added.split(/[- :]/);
+			var pdateadd = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+			today=new Date();
+			var one_day=1000*60*60*24;
+			dayscount = Math.ceil((today.getTime()-pdateadd.getTime())/(one_day));
+			if (dayscount < 30)
+			{
+			picons = picons + '<img src="img/icons/icon_new.gif">';
+			}
 			$('#productDetails').append('<li><img src="http://toyworld.in/catalog/images/' + pdetail.products_image + '" width="100%">' +
 					'<br>' + pdetail.products_description + '<br><br><img src="img/ages/a' + pdetail.products_minage + '.gif"><img src="img/ages/b' + pdetail.products_maxage + '.gif">' + picons + '<br><b>Price Rs.' + pdetail.total_price + '</b><br><br>');
 			$('#productName').append('<h2>' + pdetail.products_name + '</h2>');
