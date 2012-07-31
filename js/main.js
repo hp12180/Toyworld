7f22beaf112f9eb493a20c1f459234765b4f9ebf
Harshit Patel <hp12180@yahoo.com>
2012-07-31 17:08:51 +0530

    fd

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function getmainbanners() {
 	$('#busy').show();
 	$('#busy').hide();
 	$('#mainDet li').remove();
-	$('#mainDet').append('<div class="tile"><a href="myaccount.html">Login</a></div><div class="tile"><a href="index.html">Top Ten Toys</a></div><div class="tile"><a href="index.html">Top Ten Categories</a></div><div class="tile"><a href="index.html">Shop by Brand</a></div><div class="tile"><a href="index.html">Shop by Category</a></div><div class="tile"><a href="index.html">Shop by Age</a></div><div class="tile"><a href="index.html">Shop by Price</a></div>	<div class="tile"><a href="index.html">Contact Us</a></div><div class="tile"><a href="index.html">My Account</a></div><div class="tile"><a href="index.html">Shopping Cart</a></div>');	
+	$('#mainDet').append('<div class="tile"><a href="login.html">Login</a></div><div class="tile"><a href="index.html">Top Ten Toys</a></div><div class="tile"><a href="index.html">Top Ten Categories</a></div><div class="tile"><a href="index.html">Shop by Brand</a></div><div class="tile"><a href="index.html">Shop by Category</a></div><div class="tile"><a href="index.html">Shop by Age</a></div><div class="tile"><a href="index.html">Shop by Price</a></div>	<div class="tile"><a href="index.html">Contact Us</a></div><div class="tile"><a href="index.html">My Account</a></div><div class="tile"><a href="index.html">Shopping Cart</a></div>');	
 	setTimeout(function(){
 		scroll.refresh();
 	});
