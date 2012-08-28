2f90b648aa60d65f7915e998ff1ab5daef0265b6
Harshit Patel <hp12180@yahoo.com>
2012-08-28 10:45:01 +0530

    gh

--- js/post.js
+++ js/post.js
@@ -9,11 +9,11 @@ $(document).bind('deviceready', function(){
                 url: 'http://toyworld.in/app/services/login.php',
                 success: function(data){
                     console.log(data);
-                    alert('Your comment was successfully added');
+                    alert('You have successfully logged in.');
                 },
                 error: function(){
                     console.log(data);
-                    alert('There was an error adding your comment');
+                    alert('There was an error in your credentials');
                 }
             });
             return false;
