c53fcc2067340011b506d7073aade058cc870b0c
Harshit Patel <hp12180@yahoo.com>
2012-12-26 18:38:02 +0530

    s

--- js/post.js
+++ js/post.js
@@ -1,18 +1,14 @@
 var serviceURL = localStorage['serviceURL'];
 var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
 
-var email = getUrlVars()["email"];
-var password = getUrlVars()["password"];
-
-$(window).load(function() {
-	setTimeout(loginStat, 100);
-});
+var email = 'hp12180@yahoo.com';
+var password = 'getright';
 
 $(document).ajaxError(function(event, request, settings) {
 	$('#busy').append('Error accessing the server');
 });
 
-function loginStat() {
+$('form').submit(function(){
 	$('#busy').show();
 	$.getJSON(serviceURL + 'login.php?email=' + email + '&password=' + password, function(data) {
 		$('#busy').hide();
@@ -31,16 +27,4 @@ function loginStat() {
 			scroll.refresh();
 		});
 	});
-}
-
-function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
 }
